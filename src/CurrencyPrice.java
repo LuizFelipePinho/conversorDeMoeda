2922be02d1d14514243602a5122cb053c9b3405b
LuizFelipePinho <luizfelipesilva458@gmail.com>
2023-02-27 21:56:29 -0300

    add currency price

---
+++ src/CurrencyPrice.java
@@ -0,0 +1,57 @@
+public class CurrencyPrice {
+  private double real = 1;
+  private double euros = 5.51;
+  private double dollar = 5.21;
+  private double Libra = 6.22;
+  private double Ienes = 0.038;
+  private double WonCoreano = 0.0039;
+
+  public void setReal(double real) {
+    this.real = real;
+  }
+
+  public void setEuros(double euros) {
+    this.euros = euros;
+  }
+
+  public void setDollar(double dollar) {
+    this.dollar = dollar;
+  }
+
+  public void setLibra(double libra) {
+    this.Libra = libra;
+  }
+
+  public void setIenes(double ienes) {
+    this.Ienes = ienes;
+  }
+
+  public void setWonCoreano(double wonCoreano) {
+    this.WonCoreano = wonCoreano;
+  }
+
+  public double getReal() {
+    return real;
+  }
+
+  public double getEuros() {
+    return euros;
+  }
+
+  public double getDollar() {
+    return dollar;
+  }
+
+  public double getLibra() {
+    return Libra;
+  }
+
+  public double getIenes() {
+    return Ienes;
+  }
+
+  public double getWonCoreano() {
+    return WonCoreano;
+  }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,57 @@
 public class Main {
-  
+
   public static void main(String[] args) {
 
-    Box box = new Box();  
-    // double valueInput =  box.GetInputValue("Digite um valor");
-    // System.out.println(valueInput);
+    PerformConversion performConversion = new PerformConversion();
 
-    // String optionConvertSelected = box.optionsConverteTo();
+    int keepOptionSelectd = 0;
 
-    // String optionCoinSelected = box.optionCoinSelected();
+    while (keepOptionSelectd == 0) {
 
-    // System.out.println(optionCoinSelected);
-    // box.showMessageValueConvert(0.1);
-    int keepOptionSelectd =  box.keepConversion();
-    System.out.println(keepOptionSelectd);
+      String optionConvertSelected = performConversion.box.optionsConverteTo();
 
+      if (optionConvertSelected == "Conversor de Moeda") {
+        double valueInput = performConversion.box.getInputValue("Digite um valor");
+        performConversion.calculatorCoin.setValueCoinToConvert(valueInput);
+
+        String optionCoinSelected = performConversion.box.optionCoinSelected();
+
+        if (optionCoinSelected == "De Reais a Dolares") {
+          performConversion.realToShow(performConversion.currencyPrice.getDollar());
+
+        } else if (optionCoinSelected == "De Reais a Euros") {
+          performConversion.realToShow(performConversion.currencyPrice.getEuros());
+
+        } else if (optionCoinSelected == "De Reais a Libras") {
+          performConversion.realToShow(performConversion.currencyPrice.getLibra());
+
+        } else if (optionCoinSelected == "De Reais a Yenes") {
+          performConversion.realToShow(performConversion.currencyPrice.getIenes());
+
+        } else if (optionCoinSelected == "de Reais a Won Coreano") {
+          performConversion.realToShow(performConversion.currencyPrice.getWonCoreano());
+
+        } else if (optionCoinSelected == "De Dolares a Reais") {
+          performConversion.coinToRealShow(performConversion.currencyPrice.getDollar());
+
+        } else if (optionCoinSelected == "De Euros a reais") {
+          performConversion.coinToRealShow(performConversion.currencyPrice.getEuros());
+
+        } else if (optionCoinSelected == "De libras a Reais") {
+          performConversion.coinToRealShow(performConversion.currencyPrice.getLibra());
+
+        } else if (optionCoinSelected == "De Reais a Yenes") {
+          performConversion.coinToRealShow(performConversion.currencyPrice.getIenes());
+
+        } else if (optionCoinSelected == "De Won Coreano a Reais") {
+          performConversion.coinToRealShow(performConversion.currencyPrice.getWonCoreano());
+
+        }
+
+        keepOptionSelectd = performConversion.box.keepConversion();
+      }
+
+    }
 
   }
 }
